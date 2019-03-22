c2dbba37530d57147332d62462814844dce6e096
Riley Benson <ryguybenson@yahoo.com>
2019-03-22 10:24:16 -0700

    insert api url

--- src/doctor.js
+++ src/doctor.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
   $('#input').submit(function() {
-    let city = $('#location').val();
-    $('#location').val("");
-
+    // event.preventDefault();
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${"Portland"}&appid=[API-KEY-GOES-HERE]`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&location=45.512794%2C%20-122.679565%2C20000&user_location=45.512794%2C%20-122.679565&skip=0&limit=10&user_key=
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -19,7 +17,7 @@ $(document).ready(function() {
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      $('result').text(`These doctors in Portland can help with that: ${doctors}`);
+      $('.result').text(`These doctors in Portland can help with that: ${body.main}`);
       $('.result').text(`Doctors named "name" in Portland: ${doctors}`);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
