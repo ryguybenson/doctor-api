342c61f33719d8a8f40d974a3894a62a74b4fdb4
Riley Benson <ryguybenson@yahoo.com>
2019-03-22 12:00:16 -0700

    change url and api call format

--- src/doctor.js
+++ src/doctor.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // event.preventDefault();
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&location=45.512794%2C%20-122.679565%2C20000&user_location=45.512794%2C%20-122.679565&skip=0&limit=10&user_key=
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=toothache&location=45.512794%2C%20-122.679565%2C20000&user_location=45.512794%2C%20-122.679565&skip=0&limit=10&user_key=9c0c734f504dc68cfe5db26fcfae17c1`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -5,5 +5,27 @@ import './sass/styles.scss';
 
 
 $(document).ready(function(){
+  $('#submit').click(function() {
+    // event.preventDefault();
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.512794%2C%20-122.679565&skip=0&limit=10&user_key=9c0c734f504dc68cfe5db26fcfae17c1`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
 
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $('.result').text(`These doctors in Portland can help with that: ${body.main}`);
+    }, function(error) {
+      $('.error').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
 });
