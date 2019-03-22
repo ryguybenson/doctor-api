261095cc646eca3c8d1af8e637744eb510f098e7
Riley Benson <ryguybenson@yahoo.com>
2019-03-22 12:13:05 -0700

    change to jquery method

--- src/main.js
+++ src/main.js
@@ -4,28 +4,47 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './sass/styles.scss';
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
+//   $('#submit').click(function() {
+//     // event.preventDefault();
+//     let promise = new Promise(function(resolve, reject) {
+//       let request = new XMLHttpRequest();
+//       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.512794%2C%20-122.679565&skip=0&limit=10&user_key=9c0c734f504dc68cfe5db26fcfae17c1`;
+//       request.onload = function() {
+//         if (this.status === 200) {
+//           resolve(request.response);
+//         } else {
+//           reject(Error(request.statusText));
+//         }
+//       }
+//       request.open("GET", url, true);
+//       request.send();
+//     });
+//
+//     promise.then(function(response) {
+//       let body = JSON.parse(response);
+//       $('.result').text(`These doctors in Portland can help with that: ${body.main}`);
+//     }, function(error) {
+//       $('.error').text(`There was an error processing your request: ${error.message}`);
+//     });
+//   });
+// });
+$(document).ready(function() {
   $('#submit').click(function() {
-    // event.preventDefault();
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.512794%2C%20-122.679565&skip=0&limit=10&user_key=9c0c734f504dc68cfe5db26fcfae17c1`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
+    // let city = $('#location').val();
+    // $('#location').val("");
+    $.ajax({
+      url: `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.512794%2C%20-122.679565&skip=0&limit=10&user_key=9c0c734f504dc68cfe5db26fcfae17c1`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.result').text(`The humidity in is ${response.main}%`);
+      },
+      error: function() {
+        $('.error').text("There was an error processing your request. Please try again.");
       }
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      $('.result').text(`These doctors in Portland can help with that: ${body.main}`);
-    }, function(error) {
-      $('.error').text(`There was an error processing your request: ${error.message}`);
     });
   });
 });
