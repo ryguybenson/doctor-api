6d7bd57d0a9745e0245d4ec35289445c071f0706
Riley Benson <ryguybenson@yahoo.com>
2019-03-31 21:44:56 -0700

    change how error message is displayed on the html

--- src/main.js
+++ src/main.js
@@ -15,33 +15,30 @@ $(document).ready(function() {
       let body = JSON.parse(response);
       // console.log(body);
       if(body.data.length < 1) {
-      $("#showErrors").text(`There was an error processing your request. Please try a different sympton:`)
+        $("#nameResults").text(`There was an error processing your request. Please try a different sympton:`)
       } else {
-      let firstName = [];
-      let lastName = [];
-      let street = [];
-      let city = [];
-      let state = [];
-      let zip = [];
-      let phoneNumber = [];
-      var info = [];
+        let firstName = [];
+        let lastName = [];
+        let street = [];
+        let city = [];
+        let state = [];
+        let zip = [];
+        let phoneNumber = [];
+        var info = [];
 
-      for(let i = 0; i < body.data.length; i++) {
-      firstName = body.data[i].profile.first_name;
-      lastName = body.data[i].profile.last_name;
-      street =body.data[i].practices[0].visit_address.street;
-      city =body.data[i].practices[0].visit_address.city;
-      state =body.data[i].practices[0].visit_address.state;
-      zip =body.data[i].practices[0].visit_address.zip;
-      phoneNumber =body.data[i].practices[0].phones[0].number;
-       info.push('<li>'+ firstName + " " + lastName  +'</li>'+ '<p>'+ street + '<br>' + city + " " + state + " " + zip + '<br>' + "Phone number: "+phoneNumber +'</p>')
-       // console.log("this is info" + info);
+        for(let i = 0; i < body.data.length; i++) {
+        firstName = body.data[i].profile.first_name;
+        lastName = body.data[i].profile.last_name;
+        street =body.data[i].practices[0].visit_address.street;
+        city =body.data[i].practices[0].visit_address.city;
+        state =body.data[i].practices[0].visit_address.state;
+        zip =body.data[i].practices[0].visit_address.zip;
+        phoneNumber =body.data[i].practices[0].phones[0].number;
+         info.push('<li>'+ firstName + " " + lastName  +'</li>'+ '<p>'+ street + '<br>' + city + " " + state + " " + zip + '<br>' + "Phone number: "+phoneNumber +'</p>')
+         // console.log("this is info" + info);
+        }
+          $('#nameResults').html(info);
       }
-        $('#nameResults').html(info);
-      }
-      },
-      function(error){
-        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
       });
     });
   });
