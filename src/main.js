b9ed6667c9705148b1d9463da5ae75ff4c05621c
Riley Benson <ryguybenson@yahoo.com>
2019-03-31 21:11:42 -0700

    split logic and find correct API paths for API calls

--- src/main.js
+++ src/main.js
@@ -1,51 +1,47 @@
-import $ from 'jquery';
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './sass/styles.scss';
+import $ from 'jquery';
+import '../node_modules/bootstrap/scss/bootstrap.scss';
+import { APIDoctor } from './doctor.js';
 
-
-$(document).ready(function(){
-  $('#submit').click(function() {
-    // event.preventDefault();
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.512794%2C%20-122.679565&skip=0&limit=10&user_key=9c0c734f504dc68cfe5db26fcfae17c1`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      console.log(body);
-      $('.result').text(`These doctors in Portland can help with that: ${body.main}`);
-    }, function(error) {
-      $('.error').text(`There was an error processing your request: ${error.message}`);
-    });
-  });
-});
 $(document).ready(function() {
-  $('#submit').click(function() {
-    // let city = $('#location').val();
-    // $('#location').val("");
-    $.ajax({
-      url: `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&user_location=45.512794%2C%20-122.679565&skip=0&limit=10&user_key=9c0c734f504dc68cfe5db26fcfae17c1`,
-      type: 'GET',
-      data: {
-        format: 'json'
-      },
-      success: function(response) {
-        $('.result').text(`${response.main}`);
-      },
-      error: function() {
-        $('.error').text("There was an error processing your request. Please try again.");
+  $("#doctor-form").submit(function(event) {
+    event.preventDefault();
+    const symptomInput = $("#symptom").val();
+    const doctorInput = $("#doctor").val();
+    const findDoctorInfo = new APIDoctor();
+    let doctorInfoOutput = findDoctorInfo.doctorInfo(symptomInput, doctorInput);
+
+    doctorInfoOutput.then(function(response) {
+      let body = JSON.parse(response);
+      // console.log(body);
+      if(body.data.length < 1) {
+      $("#showErrors").text(`There was an error processing your request. Please try a different sympton:`)
+      } else {
+      let firstName = [];
+      let lastName = [];
+      let street = [];
+      let city = [];
+      let state = [];
+      let zip = [];
+      let phoneNumber = [];
+      var info = [];
+
+      for(let i = 0; i < body.data.length; i++) {
+      firstName = body.data[i].profile.first_name;
+      lastName = body.data[i].profile.last_name;
+      street =body.data[i].practices[0].visit_address.street;
+      city =body.data[i].practices[0].visit_address.city;
+      state =body.data[i].practices[0].visit_address.state;
+      zip =body.data[i].practices[0].visit_address.zip;
+      phoneNumber =body.data[i].practices[0].phones[0].number;
+       info.push('<li>'+ firstName + " " + lastName  +'</li>'+ '<p>'+ street + '<br>' + city + " " + state + " " + zip + '<br>' + "Phone number: "+phoneNumber +'</p>')
+       // console.log("this is info" + info);
       }
+        $('#nameResults').html(info);
+      }
+      },
+      function(error){
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      });
     });
   });
-});
